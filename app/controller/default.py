7ffb5ff80a9e13e55d6d9a0ffafb125d171fe3a7
Vlademir <vfrancisco@indracompany.com>
2022-03-21 08:16:21 -0300

    Creating folder templates replice view

--- app/controller/default.py
+++ app/controller/default.py
@@ -3,6 +3,6 @@ from app import app, db
 from app.model.entities import Pessoa
 
 @app.route('/')
-@app.route('/listagem')
-def listagem():
-	return "<h1>Sejam Bem Vindos </h1>"
+@app.route('/home')
+def home():
+	return render_template('home.html')
