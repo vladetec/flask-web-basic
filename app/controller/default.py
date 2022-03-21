0893680da7ef4a225e258a45b5175b98206975cc
Vlademir <vfrancisco@indracompany.com>
2022-03-21 09:25:46 -0300

    Creating simple listing

--- app/controller/default.py
+++ app/controller/default.py
@@ -3,6 +3,11 @@ from app import app, db
 from app.model.entities import Pessoa
 
 @app.route('/')
-@app.route('/home')
-def home():
-	return render_template('home.html')
+@app.route('/listagem')
+def listagem():
+	lista_pessoas = [
+		{'id': 1, 'nome': "Maria Silva", 'idade': 30, 'sexo': 'F', 'salario': 5000},
+		{'id': 2, 'nome': "Jose Silva", 'idade': 25, 'sexo': 'M', 'salario': 3000},
+		{'id': 3, 'nome': "Joao Silva", 'idade': 20, 'sexo': 'M', 'salario': 2000}
+	]
+	return render_template('home.html', pessoas=lista_pessoas, ordem='ID' )
